2848f54c613c754d4d18018404963fd33518d6c5
jutrasd <jutrasd7@gmail.com>
2026-04-05 21:45:34 -0400

    Implemented the Map class that takes a file path as an argument, uses meaningful data I/O (reading the csv file), and populates useful map attributes like grid data, dimensions, and obstacle data using the advanced Python library NumPy.

--- environment.py
+++ environment.py
@@ -1,8 +1,8 @@
 # environment.py
-
+import numpy as np # Import numpy library
 import csv # Import built-in csv module
 
-def write_csv():
+def create_map():
     """
     Create the csv file with map information writing to 'map_config.csv'. 0 is for empty space and 1 is for obstacles.
     """
@@ -14,4 +14,50 @@ def write_csv():
     with open('./map_config.csv', "w") as fo:
         writer = csv.writer(fo)
         writer.writerows(data)
+
+class Map:
+    """
+    A representation of the environment. Contains attributes such as grid data, dimensions, and obstacle data.
+    """
+    def __init__(self, file_path):
+        """
+        Initialize the workspace based on the provided file.
+        """
+        self.file_path = file_path
+        self.grid_data = None # Will be populated w/ numpy
+        self.dimensions =  (0, 0) # Immutable tuple
+        self.obstacles = set() # Mutable set
+
+        # Load the map on initiation
+        self.load_map()
+
+    def load_map(self):
+        """
+        Read the csv file to populate the grid data, dimensions, and obstacle data.
+        """
+        # Requirement 5: Meaningful data I/O - reading a file
+        raw_data = []
+        with open(self.file_path, "r") as fo:
+            reader = csv.reader(fo)
+            for row in reader:
+                # Convert the string digits to integers using list comprehension
+                raw_data.append([int(cell) for cell in row])
+        # Requirement 3 (1/2): Use advanced Python library numpy for data processing
+        self.grid_data = np.array(raw_data)
+
+        # Requirement 7 (1/4): Populate the dimensions (given by .shape in numpy) as an immutable tuple
+        self.dimensions = self.grid_data.shape
+
+        # Find the obstacle coordinates
+        # Use numpy's .nditer to iterate the grid data array and access indicies w/ multi_index
+        it = np.nditer(self.grid_data, flags=['multi_index'])
+        # Requirement 7 (2/4): Use list comprehension to get the obstacle coordinates of indicies with a value of 1 (an obstacle)
+        self.obstacles = {it.multi_index for x in it if x == 1}
+
+    def __str__(self):
+        """
+        Overload print() to print useful map information.
+        """
+        return f"Map loaded: {self.dimensions[0]}x{self.dimensions[1]} grid with {len(self.obstacles)} obstacles."
+print(Map('map_config.csv'))
 
