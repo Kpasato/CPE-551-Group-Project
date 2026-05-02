b5abffc893ba3ef8bb716d633fcb78d762d441ad
Kpasato <90589157+Kpasato@users.noreply.github.com>
2026-05-02 17:06:46 -0400

    adding robot tests

---
+++ tests/test_agent.py
@@ -0,0 +1,20 @@
+# test_agent.py
+from src.environment import Map, create_map
+from src.agent import Robot
+def test_robot_distance_calculation():
+    """
+    Testing that the robot correctly calculates the euclidean distance to the target
+    """
+    create_map()
+    test_map =Map("data/map_config.csv")
+    robot = Robot("R1", (0,0), (3, 4), test_map)
+    assert robot.calculate_distance() == 5.0
+def test_robot_detects_blocked_path():
+    """
+    test that the robot detects a path that passes through an obstacle to avoid collision
+    """
+    create_map()
+    test_map =Map("data/map_config.csv")
+    robot=Robot("R1",(0,0), (3, 4), test_map)
+    robot.path=[(0, 0), (0,1), (0, 2), (1, 2),(2,2), (3, 2),(3, 3), (3, 4)]
+    assert robot.is_path_clear() == False
